335ddaa716d9d6b822076b8c6d259a1d27ce1dce
Damien <damienmgilbert@gmail.com>
2019-11-01 22:45:26 -0500

    Update Receiver.ino

--- Controller/Receiver.ino
+++ Controller/Receiver.ino
@@ -1,151 +1,160 @@
-/*Libraries*/
-#include <SPI.h>  //Built-in Serial-Peripherial Interface
-#include <RF24.h>  //https://www.arduinolibraries.info/libraries/rf24
-#include <nRF24L01.h>  //Part of RF24
-#include "RoboClaw.h"
-#include <Wire.h>
-
-/*This program was runned using the nRF24L01 TRANSCEIVER...
- * If we wish to use the nRF24L01 + PA + LNA we will need to get a 3.3V voltage regulator that
- * can handle 250mA (per Arduino) unless we need more than three radios, then we should use a 
- * 3.3V voltage regulator rate at 800mA for overhead...
+/* This is the reciever code for Project Stella
+ * Contributors: Damien Gilbert, Dan Even
+ * Last Date Modified: 10/27/2019
  */
- 
-#define address2 0x80
-// Roboclaw Object
+/*Libraries*/
+#include <SPI.h>      // SPI
+#include <nRF24L01.h> // RF Transciever
+#include <RF24.h>     // RF Transciever
+#include "RoboClaw.h" // Roboclaw 
+#include <Wire.h>     // Wire
+
+//============================================================================================================
+
+/*Global Variable*/
+#define CE_PIN  7     // Chip Enable to digital pin 7
+#define CSN_PIN 8     // Chip Select Not to digital pin 8
+#define address2 0x80 // Address to roboclaw (must match inside Motion Studio)
+
+//============================================================================================================
+
+const byte thisSlaveAddress[5] = {'R','x','A','A','A'};  //Address for master (transmitter)
+const int MAX = 1024;       // Max value of joysticks, 10bit resolution
+float k = 0.01;            // Determines "steepness" of logistic curve
+float x_0 = 512;            // Initial value of joysticks (middle)
+float upper_bound = 32767;  // Maximum upper bound for logistic curve
+float lower_bound = -32767;      // Maximum lower bound for logistic curve
+float e = 2.718281828459045;// Approximate value for the 'e'
+int table[1024];            // Initialized table array
+bool newData = false;
+//bool valid;               // Boolean for reading voltage from roboclaw
+
+//============================================================================================================
+
+// Instances
 RoboClaw roboclaw(&Serial3, 10000); //Connecting TX, RX to roboclaw object
+RF24 radio(CE_PIN, CSN_PIN);
 
-/* Global */
-const byte address[6] = "00001"; // address for channel
-unsigned long lastReceiveTime = 0;  // time of last received message
-unsigned long currentTime = 0;  // current time
-
-/* Instance */
-RF24 radio(7, 8); //(digital pin CE, digital pin CSN) creates an instance of the rf24 driver
+//============================================================================================================
 
 /*Data Structure*/
 struct Data_Package {
-  byte j1PotY;  // Holds the value from joystick 1
-  byte j2PotY;  // Holds the value from joystick 2
+  int rightTrack, leftTrack;
 };
-
 Data_Package data; //Create a variable with the above structure
 
-/*Setup function, only runs onces*/
+//============================================================================================================
+
 void setup() {
-  Serial.begin(9600);  //Communication with serial montiore(baudrate)
-  Serial3.begin(38400);
-  radio.begin();  //Communication with radio
-  radio.openReadingPipe(0, address);  //Begin reading radio
-  radio.setAutoAck(false);
-  radio.setDataRate(RF24_250KBPS);
-  radio.setPALevel(RF24_PA_HIGH);  //Set power level
-  radio.startListening();  //Start listening for channel (Slave)
-  resetData(); //Reset data on setup()
+    Serial.begin(57600);     // Set baud rate for serial communication
+    Serial3.begin(38400);   // Set baud rate for serial communication to roboclaw (must match inside Motion Studio)
+
+    Serial.println("Rx Starting");  // Beginning Prompt
+    radio.begin();  // Begin radion communication
+    radio.setDataRate( RF24_250KBPS );  // Set data rate to 250kilobits per second
+    radio.openReadingPipe(1, thisSlaveAddress);  // Begin listening radio communication
+    radio.startListening();   // Radio begins listening
+
+    buildTable();  //Build logistic table for joystick mapping
 }
+
+//============================================================================================================
 
 void resetData() {
   // Reset the values when there is no radio connection - Set initial default values
-  data.j1PotY = 127; //Set to middle
-  data.j2PotY = 127; //Set to middle
+  data.rightTrack = 512; //Set to middle
+  data.leftTrack = 512; //Set to middle
 }
-void right(){
-    /*Joystick Right Hand*/
-    /*Middle: means roboclaw should have 0 (no movement)*/
-    if(data.j2PotY >= 91 && data.j2PotY <= 191){
-      data.j2PotY = 0;
-      roboclaw.ForwardM2(address2, data.j2PotY);
-      delay(10);
-    }
-    /*Middle/High: means roboclaw should have 64 (going forward at half speed)*/
-    else if(data.j2PotY >= 192 && data.j2PotY <= 254){
-      data.j2PotY = 64;
-      roboclaw.ForwardM2(address2, data.j2PotY);
-      delay(10);
-    }
-    /*High: means roboclaw should have 127 (going forward at full speed)*/
-    else if(data.j2PotY == 255){
-      data.j2PotY = 127;
-      roboclaw.ForwardM2(address2, data.j2PotY);
-      delay(10);
-    }
-    /*Middle/Low: means roboclaw should have 64 (going backwards at half speed)*/
-    else if(data.j2PotY <= 90 && data.j2PotY >= 1){
-      data.j2PotY = 64;
-      roboclaw.BackwardM2(address2, data.j2PotY);
-      delay(10);
-    }
-    /*Low: means roboclaw should have 127 (going backwards at full speed)*/
-    else if(data.j2PotY == 0){
-      data.j2PotY = 127;
-      roboclaw.BackwardM2(address2, data.j2PotY);
-      delay(10);
-    }
-    else{
-      resetData();  //Reset data
-    }
-}
-void left(){
-    /*Joystick Left Hand*/
-    /*Middle: Means roboclaw should have 0 (no movement)*/
-    if(data.j1PotY >= 91 && data.j1PotY <= 191){
-      data.j1PotY = 0;
-      roboclaw.ForwardM1(address2, data.j1PotY);
-      delay(10);
-    }
-    /*Middle/High: Means roboclaw should have 64 (going forward at half speed)*/
-    else if(data.j1PotY >= 192 && data.j1PotY <= 254){
-      data.j1PotY = 64;
-      roboclaw.ForwardM1(address2, data.j1PotY);
-      delay(10);
-    }
-    /*High: Means roboclaw should have 127 (going forward at full speed)*/
-    else if(data.j1PotY == 255){
-      data.j1PotY = 127;
-      roboclaw.ForwardM1(address2, data.j1PotY);
-      delay(10);
-    }
-    /*Middle/Low: Means roboclaw should have 64 (going backwards at half speed)*/
-    else if(data.j1PotY <= 90 && data.j1PotY >= 1){
-      data.j1PotY = 64;
-      roboclaw.BackwardM1(address2, data.j1PotY);
-      delay(10);
-    }
-    /*Low: means roboclaw should have 127 (going backwards at full speed)*/
-    else if(data.j1PotY == 0){
-      data.j1PotY = 127;
-      roboclaw.BackwardM1(address2, data.j1PotY);
-      delay(10);
-    }
-    else{
-      resetData(); //Reset Data
-    }
-}
-/*Loop is like main()*/
-void loop() {
-  // Check whether we keep receiving data, or we have a connection between the two modules
-  currentTime = millis();
-  if (currentTime - lastReceiveTime > 1000) {
-    // If current time is more then 1 second since we have received the last data, that means we have lost connection
-    resetData(); // If connection is lost, reset the data. It prevents unwanted behavior
-  }
-  if (radio.available()) {
-    radio.read(&data, sizeof(Data_Package)); //Read the whole data and store it into the 'data' structure
-    lastReceiveTime = millis(); // at this moment we have received the data
-    }
-    // Check whether we keep receiving data, or we have a connection between the two modules
-    currentTime = millis();
 
-    /*Call the motor controller algorithms*/
-    left();  // Control left motor 
-    delay(10);
-    right(); // Control right motor
-    delay(10);
-    
-    /*Table to Display Values on Serial Monitor*/
-    Serial.print("j1PotY: ");
-    Serial.print(data.j1PotY);
-    Serial.print("\t");
-    Serial.print("; j2PotY: ");
-    Serial.println(data.j2PotY);
+//============================================================================================================
+
+int logistic(float input) {
+    //returns nearest integer for logistic sigmoid function 
+    //with ranges and constants declared as globals up top
+    int result = (lower_bound + (upper_bound - lower_bound)/(1+pow(e, (-k*(input-x_0)))));
+    return result;
+}
+
+//============================================================================================================
+
+void buildTable() {
+  //Create lookup table for sigmoid map 
+  for (int i = 0; i < MAX; ++i) {
+    table[i] = logistic(i);
+    //Printable Table (comment in for testing, comment out for running)
+//    Serial.print("f(");
+//    Serial.print(i);
+//    Serial.print(")= ");
+//    Serial.println(table[i]);
+  }
+}
+
+//============================================================================================================
+
+void loop() {
+    //Call all functions to main loop
+    getData();
+    processData();
+    showData();
+    sendMotorControl();
+}
+
+//============================================================================================================
+
+void getData() {
+    if ( radio.available() ) {
+        radio.read( &data, sizeof(data) );
+        newData = true;
+    }
+}
+
+//============================================================================================================
+
+void processData() {
+  //Pull sigmoid values from lookup table where table[x] = f(x)
+  data.rightTrack = table[data.rightTrack];
+  data.leftTrack = table[data.leftTrack];
+
+  // Deadband for the Middle
+  /*(if(data.rightTrack < 10 && data.rightTrack > -10){
+    data.rightTrack = 0;
+  }
+  if(data.leftTrack < 10 && data.leftTrack > -10){
+    data.leftTrack = 0;
+  }*/
+  
+//  // Fix backward values
+//  if(data.rightTrack < 0){
+//    data.rightTrack = 32767 + abs(data.rightTrack);
+//  }
+//  if(data.leftTrack < 0){
+//    data.leftTrack = 32767 + abs(data.leftTrack);
+//  }
+}
+
+//============================================================================================================
+
+void showData() {
+    if (newData == true) {
+        Serial.print("Data received ");
+        Serial.print(data.rightTrack);
+        Serial.print("\t");
+        Serial.println(data.leftTrack);
+
+        // Commented out since we do not need this right now
+//        Serial.print("\t");
+//        Serial.print("; Main Battery Voltage: ");
+//        Serial.println(roboclaw.ReadMainBatteryVoltage(address2 ,&valid));
+        newData = false;
+    }
+}
+
+//============================================================================================================
+
+void sendMotorControl() {
+  delay(10);
+  roboclaw.DutyM1(address2, data.rightTrack);
+  delay(10);
+  roboclaw.DutyM2(address2, data.leftTrack);
+  delay(20);
 }
